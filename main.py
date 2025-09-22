21245947ed7f08d6271f685b8f6a40a0ecf8ba7e
稲田 稜 <inada.ryo@zen-integration.com>
2025-09-22 10:49:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
-# === main.py（このファイルで「MyAI.get_move」以外は変更しないでください）===
+from typing import Tuple
+from framework import Alg3D, Board   
 
-from abc import ABC, abstractmethod
-from typing import Tuple, List
-
-Board = List[List[List[int]]]  # board[z][y][x]（0=空, 1=黒, 2=白）
-
-# 変更禁止: 親インターフェース
-class Alg3D(ABC):
-    @abstractmethod
-    def get_move(self, board: Board) -> Tuple[int, int]:
-        """(x, y) を返す。0 <= x < 4, 0 <= y < 4"""
-        ...
-
-# ここから自由にアルゴリズムを記入 ----------------------------------------
 class MyAI(Alg3D):
     def get_move(self, board: Board) -> Tuple[int, int]:
-        # ここに自由にアルゴリズムを書いてください。
-        # 必ず (x, y) を返してください（0 <= x < 4, 0 <= y < 4）。
-        # 例 return (0, 0)
-        raise NotImplementedError("ここに実装してください")
-# ----------------------------------------------------------------------
-
-# 変更禁止: サーバが呼ぶエントリポイント（削除・変更しない）
-_ai = MyAI()
-
-def get_move(board: Board) -> Tuple[int, int]:
-    return _ai.get_move(board)
+        #ここから自由にアルゴリズムを記入
