de800cbc12cecbdb5971d62c986d51335d7be883
inada <inada.ryo@zen-integration.com>
2025-09-24 18:43:09 +0900

    ドライバー入りに変更

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from typing import Tuple
-from framework import Alg3D, Board   
+from typing import List, Tuple
+from local_driver import Alg3D, Board   # ローカル検証用
+#from framework import Alg3D, Board
 
 class MyAI(Alg3D):
-    def get_move(self, board: Board) -> Tuple[int, int]:
-        #ここから自由にアルゴリズムを記入
+    def get_move(self, board: List[List[List[int]]]) -> Tuple[int, int]:
+        # ここにアルゴリズムを書く
