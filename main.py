eb6e745f4d501d57246d1febb7350e1a9d25519e
inada <inada.ryo@zen-integration.com>
2025-09-24 18:53:10 +0900

    完成形

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List, Tuple
-from local_driver import Alg3D, Board   # ローカル検証用
-#from framework import Alg3D, Board
+#from local_driver import Alg3D, Board   # ローカル検証用
+from framework import Alg3D, Board
 
 class MyAI(Alg3D):
     def get_move(self, board: List[List[List[int]]]) -> Tuple[int, int]:
